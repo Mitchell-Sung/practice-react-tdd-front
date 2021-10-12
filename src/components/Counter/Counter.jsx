9e90efc120c7946ae69d91dfa0c02611cf5446dd
Mitchell-Sung <mitchell.sung@gmail.com>
2021-10-12 04:07:56 -0600

    Test counter element for counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import { useState } from 'react';
 
 const Counter = () => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div>
-      <h1>Counter Component</h1>
+      <h3 data-testid="counter">{counter}</h3>
     </div>
   );
 };
---
+++ src/components/Counter/Counter.test.js
@@ -0,0 +1,19 @@
+import { render, screen } from '@testing-library/react';
+import Counter from './Counter.jsx';
+
+test('The counter starts at 0', () => {
+  render(<Counter />);
+  // screen object를 이용해서 원하는 element에 접근할 때 ID로 이용한다.
+
+  const counterElement = screen.getByTestId('counter');
+
+  // id가 counter인 element의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
+});
+
+/**
+  NOTE:
+  
+  expect(counterElement).toBe(0); 을 사용하게 되면 에러가 발생된다. 왜냐하면
+  toBe() 함수는 <h3 data-testid="counter">{counter}</h3> 의 전체를 검사하기 때문이다.
+ */
